9f63bd43d9ac3a76c21bc3ec062bc1f0f16c376a
Tim Shadel <github@timshadel.com>
2011-12-15 13:15:10 -0700

    Alter syntax to distinguish between views with and without variations.
    Signed-off-by: Tim Shadel <github@timshadel.com>

--- test/eco_test.js
+++ test/eco_test.js
@@ -14,10 +14,42 @@ experiment.configure({
 });
 
 vows.describe("eco").addBatch({
-    "An Eco template with the experiment module embedded": {
+    "An Eco template with a protected block": {
         topic: function () {
             var template = [
-              '<%= @feature "button color", @user_context, => %>',
+              '<%= @feature "button color/red button", @user_context, run: => %>',
+              '    <button name="red"/>',
+              '<% end %>'
+            ].join("\n");
+
+            return template;
+        },
+        'for a user that is part of the "red button" variant': {
+            topic: function (template) {
+                var context = experiment.contextFor(1);
+
+                var result = eco.render(template, {
+                  feature:   function(name, context, variation_builder) {
+                    return experiment.protect(name, context, variation_builder.run);
+                  },
+                  variation: function(name, thing) {
+                    this.__variations[name] = thing;
+                  },
+                  user_context: context
+                });
+
+                return result;
+            },
+            "should render correctly": function (result) {
+                assert.equal(result.replace(/^\s\s*/, '').replace(/\s\s*$/, ''), '<button name="red"/>');
+            }
+        }
+    },
+
+    "An Eco template with variations": {
+        topic: function () {
+            var template = [
+              '<%= @feature "button color", @user_context, variations: => %>',
               '  <% @variation "green button", => %>',
               '    <button name="green"/>',
               '  <% end %>',
@@ -38,7 +70,7 @@ vows.describe("eco").addBatch({
                   feature:   function(name, context, variation_builder) {
                     // Make this a stack later, to push nested @features ??
                     this.__variations = {};
-                    variation_builder();
+                    variation_builder.variations();
                     return experiment.protect(name, context, this.__variations);
                   },
                   variation: function(name, thing) {
@@ -54,4 +86,6 @@ vows.describe("eco").addBatch({
             }
         }
     }
+
+
 }).export(module);
