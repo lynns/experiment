a6203debca90046da72eaafa0409da28ccbf6105
Stephen Lynn <stephen.g.lynn@gmail.com>
2012-12-04 09:29:34 -0700

    Added env support for overriding experiment settings based on environment var.

--- test/indexWithEnv_test.js
+++ test/indexWithEnv_test.js
@@ -3,48 +3,13 @@ var assert = require("assert"),
     path = require("path"),
     mustache = require("mustache"),
     experiment = require("./../lib"),
+    experiment2 = require("./../lib"),
     errors = require("./../lib/errors"),
     Group = require("./../lib/group"),
     Experiment = require("./../lib/experiment");
 
 vows.describe("experiment").addBatch({
     "configureFromFile": {
-        "with a valid file in prod env": {
-            topic: function () {
-                var err;
-                
-                process.env.TARGET_ENV = 'prod';
-                try {
-                    experiment.configureFromFile(path.resolve(__dirname, "configWithEnvs.json"));
-                } catch (e) {
-                    err = e;
-                }
-
-                var self = this;
-                process.nextTick(function () {
-                    self.callback(err);
-                });
-            },
-            "should succeed": function (err) {
-                assert.ok(!err);
-            },
-            "should create the specified experiments": function (err) {
-                assert.ok(Experiment.byName("featureOne"));
-                assert.ok(Experiment.byName("featureTwo"));
-                assert.ok(Experiment.byName("featureThree"));
-                assert.ok(Experiment.byName("featureFour"));
-            },
-
-            "feature with always on by default": {
-                topic: function () {
-                    var exps = experiment.readFor(experiment.contextFor(50));
-                    return experiment.feature("featureFour", exps);
-                },
-                "should return valid variant": function (variant) {
-                    assert.isTrue(!!variant);
-                }
-            }
-        },
         "with a valid file in test env": {
             topic: function () {
                 var err;
@@ -78,6 +43,7 @@ vows.describe("experiment").addBatch({
                 },
                 "should return valid variant": function (variant) {
                     assert.isFalse(!!variant);
+                    process.env.TARGET_ENV = undefined;
                 }
             }
         }
